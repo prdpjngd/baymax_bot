1efa1cd7e3ae0786d622dc4a36b7930b9805f437
pradeep <pradeepjangid.jky@gmail.com>
2020-02-01 20:59:22 +0530

    dialog flow connection made succesfullyg

---
+++ index.py
@@ -0,0 +1,39 @@
+import requests
+import ast
+import urllib.parse
+import base64
+import os
+import json
+from flask import Flask, render_template, request, redirect, make_response
+app = Flask(__name__)
+
+
+#how to get environment varible values -->  " os.environ['S3_KEY'] "
+
+@app.route('/bot',methods = ['GET'])
+def df():
+    q = request.args.get('q')
+    s_id='gyut872638o'
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36',
+    }
+
+    params = (
+        ('q', q),
+        ('sessionId', s_id),
+    )
+
+    r=requests.get('https://console.dialogflow.com/api-client/demo/embedded/86f65547-dbee-4b4a-ae30-28d6b297f137/demoQuery', headers=headers, params=params).text
+    j=json.loads(r)
+    return j['result']['fulfillment']['speech']
+
+
+
+@app.route('/',methods = ['GET'])
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
