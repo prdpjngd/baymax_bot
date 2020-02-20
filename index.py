090334e540da2145cf16b11cfda3833994eccb64
pradeep <pradeepjangid.jky@gmail.com>
2020-02-20 19:28:33 +0530

    login with session

--- index.py
+++ index.py
@@ -3,34 +3,39 @@ import ast
 import urllib.parse
 import base64
 import os
-import json
+import json,string,random
 from flask import Flask, render_template, request, redirect, make_response, session,url_for
 app = Flask(__name__)
 app.secret_key = '767rgdb263tr'
-
 
 #how to get environment varible values -->  " os.environ['S3_KEY'] "
 
 @app.route('/bot',methods = ['GET'])
 def df():
     q = request.args.get('q')
-    if 'logout' in q:
-        session.pop('uname', None)
-        return "see you ... you are logged out"
-    else:
-        s_id='gyut872638o'
-        headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36',
-        }
+    s_id = request.args.get('s_id')
+    if s_id :
+        f = open("./sessions/"+s_id,"a+")
+        if 'logout' in q:
+            session.pop('uname', None)
+            return "see you ... you are logged out"
+        else:
+            f.write(q+"%227%")
+            f.close()
+            headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36',
+            }
 
-        params = (
-            ('q', q),
-            ('sessionId', s_id),
-        )
+            params = (
+                ('q', q),
+                ('sessionId', s_id),
+            )
 
         r=requests.get('https://console.dialogflow.com/api-client/demo/embedded/86f65547-dbee-4b4a-ae30-28d6b297f137/demoQuery', headers=headers, params=params).text
         j=json.loads(r)
         return j['result']['fulfillment']['speech']
+    else:
+        return "{Error:True,Type:Invalid Session ID,Code:507},"
 
 
 @app.route('/',methods = ['GET'])
@@ -45,11 +50,13 @@ def home():
 def login():
     uname = request.args.get('username')
     passwd = request.args.get('password')
+    N = 7
+    res = ''.join(random.choices(string.ascii_uppercase + string.digits, k = N))
     if(len(str(uname))==0 or len(str(passwd))==0):
         return "{Error:True,Type:Empty-U/P,Code:509}"
     elif uname and passwd:
         session['uname'] = uname
-        return redirect(url_for('home'))
+        return redirect(url_for('home',token=res))
     else:
         return render_template('login.html')
 
